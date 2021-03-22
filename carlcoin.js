6d0a12c71f54cbb39951cbe244f35f274d444ecb
mattguy <matthewfrankflammia@gmail.com>
2021-03-22 12:54:38 -0400

    added console logging for sanity

--- carlcoin.js
+++ carlcoin.js
@@ -620,6 +620,7 @@ client.on('message', message => {
 										if(data.users[j]["unstable"] + amount > 100 && data.users[j]["unstable"] < 100 && data.users[j]["suicide"] == 0){
 											data.users[j]["suicide"] = 1;
 											message.channel.send(`You come to your senses.`);
+											console.log(data.users[j].name + " has calmed down");
 										}
 									}
 									//if lottery win
@@ -636,6 +637,7 @@ client.on('message', message => {
 										if(data.users[j]["unstable"] + amount > 100 && data.users[j]["unstable"] < 100 && data.users[j]["suicide"] == 0){
 											data.users[j]["suicide"] = 1;
 											message.channel.send(`You come to your senses.`);
+											console.log(data.users[j].name + " has calmed down");
 										}
 									}
 									//if lottery lose
@@ -652,6 +654,7 @@ client.on('message', message => {
 										if(data.users[j]["unstable"] > 100 && data.users[j]["suicide"] == 1){
 											data.users[j]["suicide"] = 0;
 											message.channel.send(`You are starting to feel irrational.`);
+											console.log(data.users[j].name + " has become irrational");
 										}
 									}
 									data.users[j]["activity"] = Date.now();
@@ -722,6 +725,7 @@ client.on('message', message => {
 						if(data.users[j]["unstable"] + amount > 100 && data.users[j]["unstable"] < 100 && data.users[j]["suicide"] == 0){
 							data.users[j]["suicide"] = 1;
 							message.channel.send(`You come to your senses.`);
+							console.log(data.users[j].name + " has calmed down");
 						}
 					}
 					//lose chance time
@@ -747,6 +751,7 @@ client.on('message', message => {
 						if(data.users[j]["unstable"] > 100 && data.users[j]["suicide"] == 1){
 							data.users[j]["suicide"] = 0;
 							message.channel.send(`You are starting to feel irrational.`);
+							console.log(data.users[j].name + " has calmed down");
 						}
 					}
 				}
@@ -1290,6 +1295,7 @@ client.on('message', message => {
 										if(data.users[i]["unstable"] + Math.floor(wager * 2.5) > 100 && data.users[i]["unstable"] < 100 && data.users[i]["suicide"] == 0){
 											data.users[i]["suicide"] = 1;
 											message.channel.send(`You come to your senses.`);
+											console.log(data.users[i].name + " has calmed down");
 										}
 									}
 									let newData = JSON.stringify(data);
@@ -1309,6 +1315,7 @@ client.on('message', message => {
 									if(data.users[i]["unstable"] > 100 && data.users[i]["suicide"] == 1){
 										data.users[i]["suicide"] = 0;
 										message.channel.send(`You are starting to feel irrational.`);
+										console.log(data.users[i].name + " has calmed down");
 									}
 									
 									let newData = JSON.stringify(data);
@@ -1386,6 +1393,8 @@ client.on('message', message => {
 					if(data.users[blackjackParse.challIndex]["unstable"] > 100 && data.users[blackjackParse.challIndex]["suicide"] == 1){
 						data.users[blackjackParse.challIndex]["suicide"] = 0;
 						message.channel.send(`You are starting to feel irrational.`);
+						console.log(data.users[blackjackParse.challIndex].name + " has become irrational");
+
 					}
 					let newData = JSON.stringify(data);
 					fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
@@ -1471,6 +1480,7 @@ client.on('message', message => {
 					if(data.users[blackjackParse.challIndex]["unstable"] + Math.floor(blackjackParse.wager * 2) > 100 && data.users[blackjackParse.challIndex]["unstable"] < 100 && data.users[blackjackParse.challIndex]["suicide"] == 0){
 						data.users[blackjackParse.challIndex]["suicide"] = 1;
 						message.channel.send(`You come to your senses.`);
+						console.log(data.users[blackjackParse.challIndex].name + " has calmed down");
 					}
 					
 					let newData = JSON.stringify(data);
@@ -1505,6 +1515,7 @@ client.on('message', message => {
 						if(data.users[blackjackParse.challIndex]["unstable"] + Math.floor(blackjackParse.wager * 2) > 100 && data.users[blackjackParse.challIndex]["unstable"] < 100 && data.users[blackjackParse.challIndex]["suicide"] == 0){
 							data.users[blackjackParse.challIndex]["suicide"] = 1;
 							message.channel.send(`You come to your senses.`);
+							console.log(data.users[blackjackParse.challIndex].name + " has calmed down");
 						}
 						
 						let newData = JSON.stringify(data);
@@ -1527,6 +1538,7 @@ client.on('message', message => {
 						if(data.users[blackjackParse.challIndex]["unstable"] > 100 && data.users[blackjackParse.challIndex]["suicide"] == 1){
 							data.users[blackjackParse.challIndex]["suicide"] = 0;
 							message.channel.send(`You are starting to feel irrational.`);
+							console.log(data.users[blackjackParse.challIndex].name + " has become irrational");
 						}
 						
 						let newData = JSON.stringify(data);
