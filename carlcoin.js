a52f31cf97b94c8761634650c60f25d83cfc591c
MattGuy2 <matthewfrankflammia@gmail.com>
2021-03-26 16:49:35 -0400

    added player card

--- carlcoin.js
+++ carlcoin.js
@@ -556,6 +556,102 @@ client.on('message', message => {
 			}
 		}
 	}
+	//player card
+	else if(message.content === '!cc playercard'){
+		//fetch and store data
+		let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+		let data = JSON.parse(database);
+		//stores user
+		let user = message.author.username;
+		let id = message.author.id;
+		//flag
+		let notFound = true;
+		//checks for name
+		for(let i=0;i<data.users.length;i++){
+			if(data.users[i].id == id){
+				//sanity
+				let sanity = "";
+				if(isNaN(data.users[i]["unstable"])){
+					data.users[i]["unstable"] = 0;
+				}
+				if(data.users[i]["unstable"] < 10){
+					sanity = "Fine";
+				}
+				else if(data.users[i]["unstable"] > 10 && data.users[i]["unstable"] < 25){
+					sanity = "Uneasy";
+				}
+				else if(data.users[i]["unstable"] > 25 && data.users[i]["unstable"] < 50){
+					sanity = "Awful";
+				}
+				else if(data.users[i]["unstable"] > 50 && data.users[i]["unstable"] < 75){
+					sanity = "Stressed";
+				}
+				else if(data.users[i]["unstable"] > 75 && data.users[i]["unstable"] < 100){
+					sanity = "Paranoid";
+				}
+				else if(data.users[i]["unstable"] > 100 && data.users[i]["unstable"] < 200){
+					sanity = "Irrational";
+				}
+				else if(data.users[i]["unstable"] > 200){
+					sanity = "Unstable";
+				}
+				//buildings and balance
+				let balance = data.users[i].balance;
+				let homes = data.users[i]["house"];
+				if(isNaN(homes)){
+					homes = 0;
+				}
+				let apartments = data.users[i]["apartment"];
+				if(isNaN(apartments)){
+					apartments = 0;
+				}
+				let skyscrapers = data.users[i]["skyscraper"];
+				if(isNaN(skyscrapers)){
+					skyscrapers = 0;
+				}
+				//stats
+				let str = data.users[i]["STR"];
+				if(isNaN(str)){
+					str = 0;
+					data.users[i]["STR"] = 0;
+				}
+				let dex = data.users[i]["DEX"];
+				if(isNaN(dex)){
+					dex = 0;
+					data.users[i]["DEX"] = 0;
+				}
+				let con = data.users[i]["CON"];
+				if(isNaN(con)){
+					con = 0;
+					data.users[i]["CON"] = 0;
+				}
+				let inte = data.users[i]["INT"];
+				if(isNaN(inte)){
+					inte = 0;
+					data.users[i]["INT"] = 0;
+				}
+				let wis = data.users[i]["WIS"];
+				if(isNaN(wis)){
+					wis = 0;
+					data.users[i]["WIS"] = 0;
+				}
+				let chr = data.users[i]["CHR"];
+				if(isNaN(chr)){
+					chr = 0;
+					data.users[i]["CHR"] = 0;
+				}
+				let buildings = homes + apartments + skyscrapers;
+				message.channel.send(`+----------------------------\n| ${data.users[i].name}\n|   o     balance: ${balance}\n|  /|\    buildings: ${buildings}\n|  / \    sanity: ${sanity}\n+----------------------------\n| stats\n| STR: ${str}\tCON: ${con}\tWIS: ${wis}\n| DEX: ${dex}\tINT: ${inte}\tCHR: ${chr}\n+----------------------------`,{"code":true});
+				notFound = false;
+				let newData = JSON.stringify(data);
+				fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+				break;	
+			}
+		}
+		if(notFound){
+			message.channel.send('You are not registered for CC!');
+		}
+	}
 	//pay user
 	else if(message.content.startsWith('!cc pay')){
 		let chop = message.content.split(" ");
