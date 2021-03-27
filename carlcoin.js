73eaa6b06c72c95ac61702336279f3fcf30ca715
MattGuy2 <matthewfrankflammia@gmail.com>
2021-03-27 19:23:11 -0400

    added doctor

--- carlcoin.js
+++ carlcoin.js
@@ -2081,6 +2081,35 @@ client.on('message', message => {
 			}
 		}
 	}
+	//doctor
+	else if(message.content === '!cc doctor'){
+		//fetch and store data
+		let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+		let data = JSON.parse(database);
+		let id = message.author.id;
+		//checks for name
+		for(let i=0;i<data.users.length;i++){
+			if(data.users[i].id == id && !isNaN(data.users[i]["unstable"])){
+				if(data.users[i].balance - 2 < 0){
+					message.channel.send(`You don't have enough CC! (Costs 2)`);
+				}
+				else{
+					data.users[i]["unstable"] -= 50;
+					data.users[i].balance -= 2;
+					if(data.users[i]["unstable"] < 0){
+						data.users[i]["unstable"] = 0;
+					}
+					message.channel.send(`The doctor gave you some medicine; You feel a bit better.`);
+					if(data.users[i]["unstable"] + 25 >= 100 && data.users[i]["unstable"] < 100){
+						message.channel.send(`You have come to your senses`);
+					}
+				}
+				let newData = JSON.stringify(data);
+				fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+				break;
+			}
+		}
+	}
 	//check sanity
 	else if(message.content === '!cc sanity'){
 		//fetch and store data
