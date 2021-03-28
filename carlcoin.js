8f9630b8f0769e9ca0c3801799546fa3f13d4977
mattguy <matthewfrankflammia@gmail.com>
2021-03-28 16:34:56 -0400

    message fix

--- carlcoin.js
+++ carlcoin.js
@@ -2207,6 +2207,9 @@ client.on('message', message => {
 		for(let i=0;i<data.users.length;i++){
 			if(data.users[i].id == id){
 				data.users[i].name = message.author.username;
+				let newData = JSON.stringify(data);
+				fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+				message.channel.send(`Your username has been updated`);
 				break;
 			}
 		}
