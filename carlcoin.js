672d56b7a7e33b4507df9cee6c65f03645348d4f
MattGuy2 <matthewfrankflammia@gmail.com>
2021-03-31 17:32:41 -0400

    attempting to find where money got lost in econ

--- carlcoin.js
+++ carlcoin.js
@@ -2136,7 +2136,7 @@ client.on('message', message => {
 							}
 						}
 						else{
-							//str check
+							//con check
 							let bonusChance = Math.random();
 							if(isNaN(data.users[i]["CON"])){
 								data.users[i]["CON"] = 0;
@@ -2275,6 +2275,10 @@ client.on('message', message => {
 	//oh man dab lol
 	else if(message.content === '!cc dab'){
 		message.channel.send(`Oh man you are one funny guy! you did it! you hit that mf dab bruh holy shit you just fuckinnn were all like im gonna type !cc dab and see that it wont do anything! but suprise bitch! it fuckinnn dabbed LOL it did the dab! can you believe this??? it fucking dabbed!!! thats so crazy and quirky that the silly money bot had a dab feature built in! lol LOL LAMO I CANT BELIEVE IT ACTUALLY DABBED LOOL wow it dabbed you know what also dabs??? DAB PENS FOR $14.99 ON https://www.olivegarden.com`);
+	}
+	//f word
+	else if(message.content === '!cc fuck'){
+		message.channel.send(`Whoa whoa whoa whoa whoa there partner, that right there is a swear word in my Christian Carl Coin no sir I cannot let you be swearing in my CCC (Christian Carl Coin)`);
 	}
 	//help menu
 	else if(message.content === '!cc help'){
