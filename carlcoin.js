fa349b3ae5204ef6eab9ae85818dd8f25879431c
mattguy <matthewfrankflammia@gmail.com>
2021-04-13 15:16:30 -0400

    fixed issue with over draining blackjack pot

--- carlcoin.js
+++ carlcoin.js
@@ -1602,7 +1602,7 @@ client.on('message', message => {
 				if(isNaN(wager) || wager < 2){
 					message.channel.send('Invalid amount entered!').then(msg => msg.delete({timeout:5000})).catch(error => {console.log(error)});
 				}
-				else if(Math.floor((wager * .25) * 2.5) > data.blackjack){
+				else if((wager * 2.5)+wager >= data.blackjack){
 					message.channel.send('The blackjack pot doesnt have enough CC!').then(msg => msg.delete({timeout:5000})).catch(error => {console.log(error)});
 				}
 				else{
