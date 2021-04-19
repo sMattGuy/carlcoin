fb2f261f7a1eee80c5b5dacacd8dfe4e2168d62c
mattguy <matthewfrankflammia@gmail.com>
2021-04-19 13:40:34 -0400

    changed cooldown command to cd

--- carlcoin.js
+++ carlcoin.js
@@ -2450,7 +2450,7 @@ client.on('message', message => {
 			}
 		}
 	}
-	else if(message.content === '!cc cooldowns'){
+	else if(message.content === '!cc cd'){
 		//fetch and store data
 		let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
 		let data = JSON.parse(database);
@@ -2506,7 +2506,7 @@ client.on('message', message => {
 	}
 	//user help
 	else if(message.content === '!cc userHelp'){
-		message.channel.send(`use !cc join to join Carl Coin!\nuse !cc balance to see your balance\nuse !cc pay <@user> <amount> to pay another user\nuse !cc work to go to the carl mines!\nuse !cc econ to see the current economy\nuse !cc purchase <type> to purchase something\nuse !cc catalog to see all things for sale\nuse !cc sell <type> to sell a house, apartment or skyscraper!\nuse !cc userSell <@user> <type> <amount> to sell to another person\nuse !cc relax to unwind some stress from gambling\nuse !cc sanity to see how you are feeling\nuse !cc leaderboard to see everyones balance\nuse !cc audit <@user> to see their balance\nuse !cc playercard to see your characters player information\nuse !cc doctor to get some medicine for your insanity\nuse !cc name to update to your current name\nuse !cc cooldowns to see when you can do timed activities again`);
+		message.channel.send(`use !cc join to join Carl Coin!\nuse !cc balance to see your balance\nuse !cc pay <@user> <amount> to pay another user\nuse !cc work to go to the carl mines!\nuse !cc econ to see the current economy\nuse !cc purchase <type> to purchase something\nuse !cc catalog to see all things for sale\nuse !cc sell <type> to sell a house, apartment or skyscraper!\nuse !cc userSell <@user> <type> <amount> to sell to another person\nuse !cc relax to unwind some stress from gambling\nuse !cc sanity to see how you are feeling\nuse !cc leaderboard to see everyones balance\nuse !cc audit <@user> to see their balance\nuse !cc playercard to see your characters player information\nuse !cc doctor to get some medicine for your insanity\nuse !cc name to update to your current name\nuse !cc cd to see when you can do timed activities again`);
 	}
 	//caps lock
 	else if(message.content.startsWith('!CC')){
