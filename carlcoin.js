845cf9d072cf9a9674171bb7551201c9284ad0d3
MattGuy2 <matthewfrankflammia@gmail.com>
2021-04-28 13:48:20 -0400

    fixing typos

--- carlcoin.js
+++ carlcoin.js
@@ -252,10 +252,10 @@ client.on('message', message => {
 					}
 				}
 				else if(mysteryNumber > guess){
-					message.react('+23EC').then(msg => msg.delete({timeout:60000})).catch(error => {console.log(error)});
+					message.react('⏬').then(msg => msg.delete({timeout:60000})).catch(error => {console.log(error)});
 				}
 				else{
-					message.react('+23EB').then(msg => msg.delete({timeout:60000})).catch(error => {console.log(error)});
+					message.react('⏫').then(msg => msg.delete({timeout:60000})).catch(error => {console.log(error)});
 				}
 			}
 		}
