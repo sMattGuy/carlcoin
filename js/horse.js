50372017cb8449e011740c2190bc965f218de03c
MattGuy2 <matthewfrankflammia@gmail.com>
2021-05-25 22:23:33 -0400

    horse cahnges

--- js/horse.js
+++ js/horse.js
@@ -21,6 +21,7 @@ function createHorse(){
 	let specialAbility = specialType[Math.floor(Math.random() * specialType.length)];
 	let name = firstPartName[Math.floor(Math.random()*firstPartName.length)] + ' ' + secondPartName[Math.floor(Math.random()*secondPartName.length)];
 	let newHorse = {"name":`${name}`,"stamina":`${stamina}`,"speed":`${speed}`,"color":`${color}`,"height":`${height}`,"weight":`${weight}`,"gender":`${gender}`,"special":`${specialAbility}`};
+	console.log(newHorse);
 	return newHorse;
 }
 
