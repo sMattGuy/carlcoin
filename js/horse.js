3a188f426cfcecf8a0dd7f941ad735d2101db115
MattGuy2 <matthewfrankflammia@gmail.com>
2021-05-26 13:06:52 -0400

    horse changes

--- js/horse.js
+++ js/horse.js
@@ -53,9 +53,136 @@ function trainHorse(client,message){
 function breedHorse(client,message){
 	
 }
-
+//!cc horseSell user index price
 function horseSell(client,message){
-	
+	let chop = message.content.split(" ");
+	if(chop.length != 5){
+		message.channel.send('Command arguments incorrect!');
+	}
+	else{
+		let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+		let data = JSON.parse(database);
+		let seller = message.author.id;
+		let mentionOK = true;
+		let buyer = 0;
+		try{
+			buyer = getUserFromMention(client,chop[chop.length-3]).id;
+		}
+		catch(err){
+			message.channel.send('Invalid user selected!');
+			mentionOK = false;
+		}
+		if(mentionOK){
+			let resell = true;
+			if(fs.existsSync(`/home/mattguy/carlcoin/cache/${buyer}horseSell`)){
+				let sellFile = fs.readFileSync(`/home/mattguy/carlcoin/cache/${buyer}horseSell`);
+				let sellParse = JSON.parse(sellFile);
+				if(sellParse.sellEnder < Date.now()){
+					fs.unlinkSync(`/home/mattguy/carlcoin/cache/${buyer}horseSell`);
+				}
+				else{
+					resell = false;
+					message.channel.send('That person is already in an offer!');
+				}
+			}
+			if(resell){
+				let price = parseInt(chop[chop.length-1]);
+				if(buyer == seller){
+					message.channel.send('You cannot sell to yourself!');
+				}
+				else if(isNaN(price) || price < 0){
+					message.channel.send('Invalid amount entered!');
+				}
+				else{
+					let noUser = true;
+					for(let i=0;i<data.users.length;i++){
+						if(data.users[i].id == seller){
+							let noBuy = true;
+							if(!data.users[i].hasOwnProperty("horses")){
+								message.channel.send('You dont own any horses!');
+								return;
+							}
+							for(let j=0;j<data.users.length;j++){
+								if(data.users[j].id == buyer){
+									let horseIndex = parseInt(chop[chop.length-2]);
+									//check if house
+									if(horseIndex < 0 || horseIndex >= data.users[i].horses.length){
+										message.channel.send('Invalid horse selected!');
+									}
+									else if(data.users[j].balance - price < 0){
+										message.channel.send('Buyer doesnt have enough CC!');
+									}
+									else{
+										let sellEnder = Date.now() + 60000;
+										let sellInfo = {"seller":`${seller}`,"sellerIndex":`${i}`,"buyer":`${buyer}`,"buyerIndex":`${j}`,"price":`${price}`,"sellEnder":`${sellEnder}`,"type":`${horseIndex}`};
+										let jsonBattle = JSON.stringify(sellInfo);
+										fs.writeFileSync(`/home/mattguy/carlcoin/cache/${buyer}horseSell`,jsonBattle);
+										message.channel.send(`${data.users[j].name}! Type !cc horseAccept to accept ${data.users[i].name}'s offer or type !cc horseDeny to reject the offer. You have 1 minute to respond.`);
+										console.log(data.users[i].name + " trying to sell to " + data.users[j].name);
+									}
+								}
+								noBuy = false;
+								break;
+							}
+						}
+						if(noBuy){
+							message.channel.send('Buyer doesnt exist!');
+						}
+						noUser = false;
+						break;
+					}
+				}
+				if(noUser){
+					message.channel.send('You are not registered for CC!');
+				}
+			}
+		}
+	}
+}
+
+function acceptDenyHorse(client,message){
+	let personsId = message.author.id;
+	if(fs.existsSync(`/home/mattguy/carlcoin/cache/${personsId}horseSell`)){
+		let sellFile = fs.readFileSync(`/home/mattguy/carlcoin/cache/${personsId}horseSell`);
+		let sellParse = JSON.parse(sellFile);
+		if(sellParse.sellEnder < Date.now()){
+			fs.unlinkSync(`/home/mattguy/carlcoin/cache/${personsId}horseSell`);
+			message.channel.send('Time has expired to accept the offer');
+		}
+		else{
+			let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+			let data = JSON.parse(database);
+			if(message.content.startsWith('!cc denyPurchase')){
+				fs.unlinkSync(`/home/mattguy/carlcoin/cache/${personsId}horseSell`);
+				message.channel.send('You have declined the offer');
+			}
+			else if(message.content.startsWith('!cc acceptPurchase')){
+				if(data.users[sellParse.buyerIndex].balance - sellParse.price < 0){
+					message.channel.send(`You don't have enough CC!`);
+				}
+				else{
+					let price = parseInt(sellParse.price);
+					data.users[sellParse.sellerIndex].balance += price;
+					data.users[sellParse.buyerIndex].balance -= price;
+					let horse = data.users[sellParse.sellerIndex].horse[parseInt(sellParse.type)];
+					data.users[sellParse.sellerIndex].horse.splice(parseInt(sellParse.type),1);
+					if(data.users[sellParse.buyerIndex].hasOwnProperty("horses")){
+						data.users[sellParse.buyerIndex].horses.push(horse);
+					}
+					else{
+						data.users[sellParse.buyerIndex].horses = [horse];
+					}
+					data.users[sellParse.sellerIndex]["activity"] = Date.now();
+					data.users[sellParse.buyerIndex]["activity"] = Date.now();
+					let newData = JSON.stringify(data);
+					fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+					fs.unlinkSync(`/home/mattguy/carlcoin/cache/${personsId}horseSell`);
+					message.channel.send('You have accepted the offer!');
+					console.log(data.users[sellParse.sellerIndex].name + " has sold to " + data.users[sellParse.buyerIndex]);
+				}
+			}
+		}
+	}
 }
 
 function horseList(client,message){
@@ -178,6 +305,19 @@ function createHorse(){
 	let newHorse = {"name":`${name}`,"stamina":`${stamina}`,"speed":`${speed}`,"color":`${color}`,"height":`${height}`,"weight":`${weight}`,"gender":`${gender}`,"special":`${specialAbility}`,"age":`${age}`,"birthday":`${today.getDate()}`,"trainingCooldown":0,"raceCooldown":0,"timeline":"Parents:Unknown"};
 	console.log(newHorse);
 	return newHorse;
+}
+
+function getUserFromMention(client,mention) {
+	if (!mention) return;
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		return client.users.cache.get(mention);
+	}
 }
 
 //export functions
