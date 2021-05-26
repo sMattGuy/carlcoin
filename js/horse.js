571dd0b2c009a597aa2024fe26377ecd32960a6a
MattGuy2 <matthewfrankflammia@gmail.com>
2021-05-25 22:24:59 -0400

    horse cahnges

--- js/horse.js
+++ js/horse.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 const colors = ['Appaloosa','Bay','Black','Brown','Buckskin','Chestnut','Cremello','Dun','Grey','Overo','Palomino','Piebald','Roan','Skewbald','Spotted','Tobiano'];
-const gender = ['Male','Female'];
+const genderType = ['Male','Female'];
 const specialType = ['Speed Boost','Stamina Boost','Slipstream','Full Force','Vicious'];
 
 const firstPartName = ['Special','Silence','Tokai','Air','Condor','Oguri','Grass','Gold','Symboli','Taiki','Daiwa','T.M.','Narita','Hishi','Fuji','Mejiro','Seiun','Yukino','Manhattan','Tosen','Haru','Kawakami','Mejiro','Fine','Smart','Narita','Super','Inari','Nishino','Biko','Bamboo','Marvelous','Mihono','Sweep','Ines','Biwa','Sakura','Shinko','Agnes','Zenno','Meisho','Rice','Admire','Curren','Eishin','Nakayama','Mayano','Nice','King','Matikane','Ikuno','Daitaku','Twin','Seeking','Tamamo'];
@@ -17,7 +17,7 @@ function createHorse(){
 	let height = Math.random() + 1;
 	height = height.toFixed(2);
 	let weight = Math.floor(Math.random() * 1000) + 380;
-	let gender = gender[Math.floor(Math.random() * gender.length)];
+	let gender = genderType[Math.floor(Math.random() * genderType.length)];
 	let specialAbility = specialType[Math.floor(Math.random() * specialType.length)];
 	let name = firstPartName[Math.floor(Math.random()*firstPartName.length)] + ' ' + secondPartName[Math.floor(Math.random()*secondPartName.length)];
 	let newHorse = {"name":`${name}`,"stamina":`${stamina}`,"speed":`${speed}`,"color":`${color}`,"height":`${height}`,"weight":`${weight}`,"gender":`${gender}`,"special":`${specialAbility}`};
