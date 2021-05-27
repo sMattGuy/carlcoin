2f5dc7d74326de36c74de30e5f0540c817bee060
MattGuy2 <matthewfrankflammia@gmail.com>
2021-05-27 12:48:12 -0400

    leaderboard updates

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -4,27 +4,34 @@ const fs = require('fs');
 function checkLeaderboard(client,message){
 	let database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
 	let data = JSON.parse(database);
-	let numberOneName = '';
-	let numberTwoName = '';
-	let numberThreeName = '';
-	let numberOne = 0;
-	let numberTwo = 0;
-	let numberThree = 0;
 	let userArray = [];
 	for(let i=0;i<data.users.length;i++){
 		if(data.users[i].balance != 0){
 			let balance = data.users[i].balance
 			let user = data.users[i].name;
-			let userObject = {name:`${user}`,balance:`${balance}`};
+			let houseValue = 50 * parseInt(data.users[i]["house"]);
+			if(isNaN(houseValue)){
+				houseValue = 0;
+			}
+			let apartmentValue = 150 * parseInt(data.users[i]["apartment"]);
+			if(isNaN(apartmentValue)){
+				apartmentValue = 0;
+			}
+			let skyscraperValue = 250 * parseInt(data.users[i]["skyscraper"]);
+			if(isNaN(skyscraperValue)){
+				skyscraperValue = 0;
+			}
+			let combinedAssests = houseValue + apartmentValue + skyscraperValue;
+			let userObject = {name:`${user}`,balance:`${balance}`,assets:`${combinedAssests}`};
 			userArray.push(userObject);
 		}
 	}
 	userArray.sort(function (a,b){
-		return b.balance - a.balance;
+		return (b.balance + b.assets) - (a.balance + a.assets);
 	});
 	let messageBox = '';
 	for(let i=0;i<userArray.length;i++){
-		messageBox += `${i+1}. ${userArray[i].name}:${userArray[i].balance}\n`;
+		messageBox += `${i+1}. ${userArray[i].name}:${userArray[i].balance} (assets:${userArray[i].assets})\n`;
 	}
 	message.channel.send(`Leaderboard of Carl Coin\n${messageBox}`,{"code":true});
 }
