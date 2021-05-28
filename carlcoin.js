1a4985e898192e8135fd772ad9a977fe65ee90dd
MattGuy2 <matthewfrankflammia@gmail.com>
2021-05-28 14:11:10 -0400

    test race with new find function

--- carlcoin.js
+++ carlcoin.js
@@ -267,6 +267,7 @@ client.on('message', message => {
 	else if(message.content.startsWith('!cc horseRace')){
 		horse.raceHorse(client,message);
 	}
+	//name horse
 	else if(message.content.startsWith('!cc horseName')){
 		horse.nameHorse(client, message);
 	}
@@ -277,6 +278,9 @@ client.on('message', message => {
 	//summon coin
 	else if(message.content === '!cc summonGaintCoin' && message.author.id == 492850107038040095){
 		admin.summonGaintCoin(client,message);
+	}
+	else if(message.content === '!cc triggerTestRace' && message.author.id == 492850107038040095){
+		horse.testRace(client,message);
 	}
 	//caps lock
 	else if(message.content.startsWith('!CC')){
