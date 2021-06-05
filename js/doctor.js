77f29e7a3c588a3dd651e68cfce6521535ab2193
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-05 14:04:29 -0400

    doctor scales in price now

--- js/doctor.js
+++ js/doctor.js
@@ -9,8 +9,9 @@ function doctorVisit(client,message){
 	//checks for name
 	for(let i=0;i<data.users.length;i++){
 		if(data.users[i].id == id && !isNaN(data.users[i]["unstable"])){
-			if(data.users[i].balance - 2 < 0){
-				message.channel.send(`You don't have enough CC! (Costs 2)`);
+			let price = Math.floor(data.users[i].balance / 4);
+			if(data.users[i].balance - price < 0){
+				message.channel.send(`You don't have enough CC! (Costs ${price})`);
 			}
 			else{
 				let insane = false;
@@ -18,8 +19,8 @@ function doctorVisit(client,message){
 					insane = true;
 				}
 				data.users[i]["unstable"] -= 50;
-				data.users[i].balance -= 2;
-				data.welfare += 2;
+				data.users[i].balance -= price;
+				data.welfare += price;
 				if(data.users[i]["unstable"] < 0){
 					data.users[i]["unstable"] = 0;
 				}
