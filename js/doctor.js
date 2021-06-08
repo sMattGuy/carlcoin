44cd976e403fb96b1a1b77273cdde5f376297b02
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-07 20:38:03 -0400

    doctor patch again

--- js/doctor.js
+++ js/doctor.js
@@ -9,7 +9,7 @@ function doctorVisit(client,message){
 	//checks for name
 	for(let i=0;i<data.users.length;i++){
 		if(data.users[i].id == id && !isNaN(data.users[i]["unstable"])){
-			let price = Math.floor(data.users[i].balance / 4) + 2;
+			let price = Math.floor(data.users[i].balance / 6) + 10;
 			if(data.users[i].balance - price < 0){
 				message.channel.send(`You don't have enough CC! (Costs ${price})`);
 			}
@@ -18,7 +18,7 @@ function doctorVisit(client,message){
 				if(data.users[i]["unstable"] >= 100){
 					insane = true;
 				}
-				data.users[i]["unstable"] -= 151;
+				data.users[i]["unstable"] -= 250;
 				data.users[i].balance -= price;
 				data.welfare += price;
 				if(data.users[i]["unstable"] < 0){
@@ -26,7 +26,7 @@ function doctorVisit(client,message){
 				}
 				message.channel.send(`The doctor gave you some medicine; You feel a bit better.`);
 				console.log(data.users[i].name + ' used the doctor');
-				if(insane && data.users[i]["unstable"] + 151 >= 100 && data.users[i]["unstable"] < 100){
+				if(insane){
 					message.channel.send(`You have come to your senses`);
 				}
 			}
