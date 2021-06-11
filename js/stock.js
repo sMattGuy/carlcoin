2ea38f6df58bfb18ecb9adedec56612a0332cc55
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-11 18:52:14 -0400

    graph mode

--- js/stock.js
+++ js/stock.js
@@ -385,7 +385,7 @@ function stockGraph(client,message){
 	
 	data = JSON.stringify(data);
 	config = JSON.stringify(config);
-	let stockSite = `<!DOCTYPE html> <html> <head> <title> Stock History </title> <style> body, form{ margin: 0 auto; max-width:652px; overflow-x:hidden; background-color:#CCCCFF;}fieldset{ display: flex;}</style><script src="https://cdn.jsdelivr.net/npm/chart.js"></script></head><body><canvas id="myChart"></canvas><script>var data=${data}; var ctx = document.getElementById("myChart").getContext("2d"); var myNewChart = new Chart(ctx).Line(data);</script></body></html>`;
+	let stockSite = `<!DOCTYPE html> <html> <head> <title> Stock History </title> <style> body, form{ margin: 0 auto; max-width:652px; overflow-x:hidden; background-color:#CCCCFF;}fieldset{ display: flex;}</style><script src="https://cdn.jsdelivr.net/npm/chart.js"></script></head><body><canvas id="myChart"></canvas><script>const data=${data};const config = ${config};var myChart = new Chart(document.getElementById("myChart"),config);</script></body></html>`;
 	if(fs.existsSync(`/var/www/html/carlHorses/StockTimeline.html`)){
 		fs.unlinkSync(`/var/www/html/carlHorses/StockTimeline.html`);
 	}
