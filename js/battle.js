1040ad5ef3dac3999f58847cf7b410cb21967d56
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-15 17:05:41 -0400

    testing a battle

--- js/battle.js
+++ js/battle.js
@@ -1,10 +1,37 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+//enemies
+//baby
+const glassJoe = {art:`
++==============================+\n|       PATHETIC ENEMY\n|		  GLASS JOE\n|         |\\     ____\n|         | \\.-./ .-'\n|          \\ _  _(\n|          | .)(./\n|          |   \\(\n|          |     \\\n|          |  \\vvv\n|          |  |__\n|         /       -.\n+==============================+\n`,hp:10,str:1,con:5,wis:4,dex:2,intel:3,chr:0};
 
+//easy
+const lesserDevil = {art:`+==============================+\n|       ENEMY OF THE STATE\n|		 THE LESSER DEVIL\n|             (_)L|J\n|      )      (") |     (\n|      ,(. A  / \\-|   (,)\n|     )' (' \\/\\ / |  ) (.\n|    (' ),).  _W_ | (,)' )\n|   ^^^^^^^^^^^^^^^^^^^^^^^\n+==============================+\n`,hp:50,str:10,con:15,wis:5,dex:4,intel:10,chr:15};
 
-const lesserDevil = {art:`
-+==============================+\n|       ENEMY OF THE STATE\n|		 THE LESSER DEVIL\n|             (_)L|J\n|      )      (") |     (\n|      ,(. A  / \\-|   (,)\n|     )' (' \\/\\ / |  ) (.\n|    (' ),).  _W_ | (,)' )\n|   ^^^^^^^^^^^^^^^^^^^^^^^\n+==============================+\n`,hp:100,str:10,con:15,wis:5,dex:4,intel:10,chr:15};
+//normal
+const floatingHead = {art:
+`+==============================+\n|       UGLY FLOATING HEAD\n|		 THE SMILING DEVIL\n|         (\\-"^^^"-/)\n|         //^\\   /^\\\\\n|        ;/ ~_\\ /_~ \\;\n|        |  / \\Y/ \\  |\n|       (,  \\0/ \\0/  ,)\n|        |   /   \\   |\n|        | (_\\._./_) |\n|         \\ \\v-.-v/ /\n|          \\ '===' /\n|           \\_____/\n+==============================+\n`,hp:125,str:20,con:30,wis:26,dex:30,intel:20,chr:25};
 
+//hard
+const jokester = {art:
+`+==============================+\n|          KNIGHT OF HELL\n|           ARMORED DEMON\n|              .    .\n|           _..;|;__;|;\n|         ,'   ';  \\'; -.\n\n|    .--._)|    ;==,|,=='\n|      \\ @; \\_  < G," G).\n|        \\/-;,(  )  .>. )\n|           < ,-;'-.__.;'\n|             \\_  -,__,'\n\n|                   ;;;;\n+==============================+\n`,hp:200,str:50,con:75,wis:50,dex:55,intel:80,chr:75};
+
+//expert
+const king = {art:
+`+==============================+\n|          KING OF HELL\n|		     THE LYCH\n|    ,    ,    /\\   /\\\n|   /( /\\ )\\  _\\ \\_/ /_\n|   |\\_||_/| < \\_   _/ >\n|   \\______/  \\|0   0|/\n|     _\\/_   _(_  ^  _)_\n|    ( () ) / \\|V"""V|/ \\\n|      {}   \\  \\_____/  /\n|      ()   /\\   )=(   /\\\n|      {}  /  \\_/\\=/\\_/  \\\n+==============================+\n`,hp:500,str:100,con:100,wis:75,dex:70,intel:120,chr:130};
+
+//nightmare
+const tux = {art:
+`+==============================+\n|      GOD OF "BUT ITS FREE"\n|	      TUX THE PENGUIN\n|              _nnnn_\n|             dGGGGMMb\n|            @p~qp~~qMb\n|            M|@||@) M|\n|            @,----.JM|\n|           JS^\\__/  qKL\n|          dZP        qKRb\n|         dZP          qKKb\n|        fZP            SMMb\n|        HZM            MMMM\n|        FqM            MMMM\n|      __| ".        |\\dS"qML\n|      |     .       |  ' \\Zq\n|     _)      \\.___.,|     .'\n|     \\____   )MMMMMP|   .'\n|           -'        --'\n+==============================+\n`,hp:1000,str:500,con:250,wis:500,dex:150,intel:800,chr:580};
+
+//arrays
+const babyEnemies = [glassJoe];
+const easyEnemies = [lesserDevil];
+const normalEnemies = [floatingHead];
+const hardEnemies = [jokester];
+const expertEnemies = [king];
+const nightmareEnemies = [tux];
+//action bar constant
 const actionBar = `ACTIONS:\n!cc attack | !cc block\n!cc look   | !cc item\n!cc run    | !cc magic`;
 
 
@@ -64,22 +91,63 @@ function testResponses(client,message){
 		message.channel.send(`You are not registered for Carl Coin!`);
 		return;
 	}
-	//adjust health
+	//adjust health & get level
 	playerHp += con;
-	//enemy information
-	let enemy = lesserDevil;
-	let eArt = enemy.art;
-	let eHp = enemy.hp;
-	let eStr = enemy.str;
-	let eCon = enemy.con;
-	let eWis = enemy.wis;
-	let eDex = enemy.dex;
-	let eIntel =  enemy.intel;
-	let eChr = enemy.chr;
+	let playerLevel = str + con + wis + dex + intel + chr;
 	
-	//actual box that will perform the loop
-	let initMessage = `Info will be here!\n`;
-	frame(initMessage);
+	//get the difficulty level a player wants
+	const diffFilter = m => {
+		return ((m.content === 'baby' || m.content === 'easy' || m.content === 'normal' || m.content === 'hard' || m.content === 'expert' || m.content === 'nightmare')&&(message.author.id == m.author.id));
+	}
+	let enemy = {};
+	let eArt = `(O_O)`;
+	let eHp = 0;
+	let eStr = 0;
+	let eCon = 0;
+	let eWis = 0;
+	let eDex = 0;
+	let eIntel =  0;
+	let eChr = 0;
+	let enemyLevel = 0;
+	message.channel.send(`what difficulty level do you want (You are level ${playerLevel}): baby, easy, normal, hard, expert, nightmare`).then(msg => {
+		message.channel.awaitMessages(diffFilter,{max:1,time:30000,errors:['time']}).then(choice => {
+			choice.first().delete();
+			let option = choice.first().content;
+			if(option == 'baby'){
+				enemy = babyEnemies[Math.floor(Math.random() * babyEnemies.length)];
+			}
+			else if(option == 'easy'){
+				enemy = easyEnemies[Math.floor(Math.random() * easyEnemies.length)];
+			}
+			else if(option == 'normal'){
+				enemy = normalEnemies[Math.floor(Math.random() * normalEnemies.length)];
+			}
+			else if(option == 'hard'){
+				enemy = hardEnemies[Math.floor(Math.random() * hardEnemies.length)];
+			}
+			else if(option == 'expert'){
+				enemy = expertEnemies[Math.floor(Math.random() * expertEnemies.length)];
+			}
+			else if(option == 'nightmare'){
+				enemy = nightmareEnemies[Math.floor(Math.random() * nightmareEnemies.length)];
+			}
+			eArt = enemy.art;
+			eHp = enemy.hp;
+			eStr = enemy.str;
+			eCon = enemy.con;
+			eWis = enemy.wis;
+			eDex = enemy.dex;
+			eIntel =  enemy.intel;
+			eChr = enemy.chr;
+			enemyLevel = eStr + eCon + eWis + eDex + eIntel + eChr;
+			//actual box that will perform the loop
+			let initMessage = `Info will be here!\n`;
+			frame(initMessage);
+		}).catch(e => {
+			message.channel.send(`No difficulty choice made in time`);
+			console.log(e);
+		})
+	});
 	
 	async function frame(infoMessage){
 		message.channel.send(`${infoMessage}${eArt}HP:${playerHp} | ENEMY HP:${eHp}\n${actionBar}`,{code:true}).then( msg =>{
@@ -164,7 +232,17 @@ function testResponses(client,message){
 				}
 				//check hp of enemy
 				if(eHp <= 0){
-					gameMessage += `You have defeated the enemy! You win!\n`;
+					database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+					data = JSON.parse(database);
+					let amountEarned = 0;
+					for(let i=0;i<data.users.length;i++){
+						if(data.users[i].id == id){
+							data.users[i].balance += amountEarned;
+						}
+					}
+					let newData = JSON.stringify(data);
+					fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+					gameMessage += `You have defeated the enemy! You win! As a reward you got ${amountEarned}CC!\n`;
 					message.channel.send(gameMessage);
 					return;
 				}
@@ -213,7 +291,17 @@ function testResponses(client,message){
 					}
 				}
 				if(playerHp <= 0){
-					gameMessage += `You have died! So sad!\n`;
+					database = fs.readFileSync('/home/mattguy/carlcoin/database.json');
+					data = JSON.parse(database);
+					let amountEarned = 0;
+					for(let i=0;i<data.users.length;i++){
+						if(data.users[i].id == id){
+							data.users[i].balance -= amountEarned;
+						}
+					}
+					let newData = JSON.stringify(data);
+					fs.writeFileSync('/home/mattguy/carlcoin/database.json',newData);
+					gameMessage += `You have died! So sad! You lost ${amountEarned}CC!\n`;
 					message.channel.send(gameMessage);
 					return;
 				}
