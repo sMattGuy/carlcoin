bfc5ebe635f6785c6e8cae2125f26104b2714355
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-15 10:43:05 -0400

    no bob new feature time

--- js/battle.js
+++ js/battle.js
@@ -8,7 +8,7 @@ function testResponses(client,message){
 		}
 		return (m.content === '!cc attack' || m.content === '!cc look' || m.content === '!cc block' || m.content === '!cc run' || m.content === '!cc item');
 	};
-	message.channel.send('ACTIONS:\n!cc attack\n!cc look\n!cc block\n!cc run\n!cc item').then(()=>{message.channel.awaitMessages(filter,{max:1,time:20000,errors:['time']}).then(choice => {message.channel.send(`You selected ${choice}`)})}).catch(e => {message.channel.send(`Didnt get valid response in time`);console.log(e);});
+	message.channel.send('ACTIONS:\n!cc attack\n!cc look\n!cc block\n!cc run\n!cc item').then(()=>{message.channel.awaitMessages(filter,{max:1,time:20000,errors:['time']}).then(choice => {message.channel.send(`You selected ${choice.content}`)})}).catch(e => {message.channel.send(`Didnt get valid response in time`);console.log(e);});
 }
 
 module.exports = {
