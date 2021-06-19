e6168456ddb67e0a3130a346360e6031ac4dff2d
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-19 00:12:48 -0400

    connect4 attempt to fix undefined value

--- js/connect.js
+++ js/connect.js
@@ -127,7 +127,8 @@ function connect4(client,message){
 				choice.first().delete().catch(() => {console.log('couldnt delete message in battle')});
 				let action = choice.first().content;
 				console.log(action);
-				let number = parseInt(action[action.length-1]);
+				let chopAction = action.split(" ");
+				let number = parseInt(chopAction[chopAction.length-1]);
 				console.log(number);
 				if(number >= boardArray.length || number < 0 || isNaN(number)){
 					msg.delete();
