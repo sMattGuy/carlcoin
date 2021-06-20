48f3ded39ece618b3843a6f6f7a25b205a2fbf41
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-19 22:16:04 -0400

    issues with realty

--- js/realty.js
+++ js/realty.js
@@ -192,6 +192,8 @@ function realtyBuy(client,message){
 			for(let j=0;j<data.users.length;j++){
 				if(data.users[j].id == realty.list[index].id){
 					//pay seller
+					console.log(realty.list[index].id);
+					console.log(client.users.cache.get(realty.list[index].id));
 					data.users[j].balance += realty.list[index].price;
 					client.users.cache.get(realty.list[index].id).send(`Your listing of a ${realty.list[index].type} for ${realty.list[index].price}CC has been sold to ${data.users[i].name}!`).catch(() => {console.log('Failed to alert seller of their listing being sold')});
 					realty.list.splice(index,1);
