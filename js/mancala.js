1f1d1f4707c2366992ac6447b8f4443fc686f233
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-21 13:49:31 -0400

    fixing how board looks

--- js/mancala.js
+++ js/mancala.js
@@ -379,7 +379,7 @@ async function drawMancala(channel,info,boardArray,playerIcon,EnemyIcon,playerSc
 			for(let k=0;k<boardArray[i][j];k++){
 				let currentRock = `${pieceNames[k%pieceNames.length]}Rock.png`;
 				console.log(currentRock);
-				let rock = await Canvas.loadImage(`/home/mattguy/carlcoin/mancala/currentRock`);
+				let rock = await Canvas.loadImage(`/home/mattguy/carlcoin/mancala/${currentRock}`);
 				ctx.drawImage(rock,90 + (i*120) + Math.floor(Math.random * 3),100 + (j * 45) + Math.floor(Math.random * 3));
 			}
 			ctx.font = '12px sans-serif';
