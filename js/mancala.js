8c7d20d636c5f01257f7a04940e37dc3f029c42d
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-21 14:00:42 -0400

    fixing how board looks

--- js/mancala.js
+++ js/mancala.js
@@ -379,7 +379,7 @@ async function drawMancala(channel,info,boardArray,playerIcon,EnemyIcon,playerSc
 			for(let k=0;k<boardArray[i][j];k++){
 				let currentRock = `${pieceNames[k%pieceNames.length]}Rock.png`;
 				let rock = await Canvas.loadImage(`/home/mattguy/carlcoin/mancala/${currentRock}`);
-				ctx.drawImage(rock,90 + ((i*120) + Math.floor(Math.random() * 3)),100 + ((j * 45) + Math.floor(Math.random() * 3)),10,10);
+				ctx.drawImage(rock,85 + ((i*120) + Math.floor(Math.random() * 3)),90 + ((j * 45) + Math.floor(Math.random() * 7)),10,10);
 			}
 			ctx.font = '12px sans-serif';
 			ctx.fillStyle = '#000000';
@@ -388,11 +388,11 @@ async function drawMancala(channel,info,boardArray,playerIcon,EnemyIcon,playerSc
 	}
 	for(let k=0;k<playerScore;k++){
 		let rock = await Canvas.loadImage(`/home/mattguy/carlcoin/mancala/${pieceNames[k%pieceNames.length]}Rock.png`);
-		ctx.drawImage(rock,150 + Math.floor(Math.random() * 60),50 + Math.floor(Math.random * 10),10,10);
+		ctx.drawImage(rock,150 + Math.floor(Math.random() * 60),50 + Math.floor(Math.random() * 10),10,10);
 	}
 	for(let k=0;k<enemyScore;k++){
 		let rock = await Canvas.loadImage(`/home/mattguy/carlcoin/mancala/${pieceNames[k%pieceNames.length]}Rock.png`);
-		ctx.drawImage(rock,150 + Math.floor(Math.random() * 60),340 + Math.floor(Math.random * 10),10,10);
+		ctx.drawImage(rock,150 + Math.floor(Math.random() * 60),340 + Math.floor(Math.random() * 10),10,10);
 	}
 	//draw scores
 	ctx.font = '12px sans-serif';
