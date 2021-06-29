ae35f26c96145bb5e963bc521b45f7a357c44b09
MattGuy2 <matthewfrankflammia@gmail.com>
2021-06-29 16:34:42 -0400

    fixing mancala as well

--- js/mancala.js
+++ js/mancala.js
@@ -134,6 +134,7 @@ function mancala(client,message){
 				}
 				if(data.users[i].id == id){
 					data.users[i].busy = 0;
+					data.users[i].balance += wager;
 				}
 			}
 			newData = JSON.stringify(data);
